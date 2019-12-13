79527033fcb22e45858dbb26980f70005c6509e5
Edwin Flórez <edwin@sonartelematics.com>
2019-12-13 16:01:32 -0500

    Adds react client base

---
+++ CheckMyVehicle/checkmyvehicleclient/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./App.css";
+import AppRouter from "./router/Index";
+
+const App: React.FC = () => {
+    return (
+        <AppRouter />
+    );
+}
+export default App;
